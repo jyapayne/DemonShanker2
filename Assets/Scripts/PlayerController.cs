609f4e9269ede2149dc97c81a5b311f698919b2e
shakurass <shakurass@gmail.com>
2014-04-25 23:22:27 -0600

    Player controller implemented.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	void Update() {
+
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.MovePosition (rigidbody.position + movement * speed * Time.deltaTime);
+
+		rigidbody.freezeRotation = true;
+	}
+}
