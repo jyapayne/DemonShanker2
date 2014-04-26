828a0b50ddbc1275cce75e4281d885337a3985fc
Wilson Leong <wilson@binaryrebellion.com>
2014-04-26 14:47:56 -0600

    Patrons have destination list

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Driver : MonoBehaviour {
+
+    Vector3 dest;
+
+    void Awake() {
+        Vector3 pos = GameObject.Find("node3").transform.position;
+        NavMeshAgent nva = GetComponent<NavMeshAgent>();
+        nva.SetDestination(pos);
+        nva.updateRotation = false;
+    }
+}
---
+++ Assets/Scripts/NavPoint.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavPoint : MonoBehaviour {
+    void OnTriggerEnter(Collider other) {
+        Patron p = other.gameObject.GetComponent<Patron>();
+        if(p != null)
+            p.NextDest();
+    }
+}
